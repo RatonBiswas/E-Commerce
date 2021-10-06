aff3685bab9b51210ec8b760584d41db9c979294
Raton Biswas <32263412+RatonBiswas@users.noreply.github.com>
2021-10-06 17:15:03 +0600

    change some unformat data

--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -2,8 +2,8 @@ import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
 import styled from "styled-components";
 
 const Container = styled.div`
-  height: 100vh;
   width: 100%;
+  height: 100vh;
   display: flex;
   position: relative;
 `;
@@ -13,63 +13,119 @@ const Arrow = styled.div`
   height: 50px;
   background-color: #fff7f7;
   border-radius: 50%;
-  align-items: center;
   display: flex;
+  align-items: center;
   justify-content: center;
   position: absolute;
   top: 0;
-  margin: auto;
   bottom: 0;
   left: ${(props) => props.direction === "left" && "10px"};
   right: ${(props) => props.direction === "right" && "10px"};
-  opacity: 0.5;
+  margin: auto;
   cursor: pointer;
+  opacity: 0.5;
+  z-index: 2;
 `;
+
 const Wrapper = styled.div`
   height: 100%;
+  display: flex;
+  transition: all 1.5s ease;
+  transform: translateX(${(props) => props.slideIndex * -100}vw);
 `;
 
 const Slide = styled.div`
-  width:100vw;
-  height:100vh
+  width: 100vw;
+  height: 100vh;
   display: flex;
   align-items: center;
+  background-color: #${(props) => props.bg};
 `;
-const ImageContainer = styled.div`
-  flex: 1;
+
+const ImgContainer = styled.div`
   height: 100%;
+  flex: 1;
 `;
+
 const Image = styled.img`
   height: 80%;
 `;
+
 const InfoContainer = styled.div`
   flex: 1;
   padding: 50px;
 `;
 
-const Title = styled.h1``
-const Description = styled.p``
-const Button = styled.button``
+const Title = styled.h1`
+  font-size: 70px;
+`;
+
+const Desc = styled.p`
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
+`;
 
 const Slider = () => {
+    const handleClick = (direction) => {
+
+    }
   return (
     <Container>
-      <Arrow direction="left">
+      <Arrow direction="left" onClick={()=> handleClick('left')}>
         <ArrowLeftOutlined />
       </Arrow>
       <Wrapper>
-        <Slide>
-          <ImageContainer>
+        <Slide bg='f5fafd'>
+          <ImgContainer>
             <Image src="https://cdn.yourpng.com/uploads/preview/model-girls-png-images-11590815772a7t4xsjrek.png" />
-          </ImageContainer>
+          </ImgContainer>
           <InfoContainer>
-              <Title></Title>
-              <Description></Description>
-              <Button></Button>
+            <Title>Summer Sale</Title>
+            <Desc>It was an easy ride</Desc>
+            <Button>SHOW NOW</Button>
+          </InfoContainer>
+        </Slide>
+        <Slide bg='fbf0f4'>
+          <ImgContainer>
+            <Image src="https://cdn.yourpng.com/uploads/preview/model-girls-png-images-11590815772a7t4xsjrek.png" />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>Morning Sale</Title>
+            <Desc>It was an easy ride</Desc>
+            <Button>SHOW NOW</Button>
+          </InfoContainer>
+        </Slide>
+        <Slide bg='fcf1ed'>
+          <ImgContainer>
+            <Image src="https://cdn.yourpng.com/uploads/preview/model-girls-png-images-11590815772a7t4xsjrek.png" />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>Spring Sale</Title>
+            <Desc>It was an easy ride</Desc>
+            <Button>SHOW NOW</Button>
+          </InfoContainer>
+        </Slide>
+        <Slide bg='f5fafd'>
+          <ImgContainer>
+            <Image src="https://cdn.yourpng.com/uploads/preview/model-girls-png-images-11590815772a7t4xsjrek.png" />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>Winter Sale</Title>
+            <Desc>It was an easy ride</Desc>
+            <Button>SHOW NOW</Button>
           </InfoContainer>
         </Slide>
       </Wrapper>
-      <Arrow direction="right">
+      <Arrow direction="right" onClick={()=> handleClick('left')}>
         <ArrowRightOutlined />
       </Arrow>
     </Container>
