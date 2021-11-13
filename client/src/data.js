e4add23441e6f0ec18b5c29832e4b92ff634b40a
Raton Biswas <32263412+RatonBiswas@users.noreply.github.com>
2021-11-13 17:51:56 +0600

    Populer Products

--- client/src/data.js
+++ client/src/data.js
@@ -40,3 +40,39 @@ export const sliderItems = [
       title: "LIGHT JACKETS",
     },
   ];
+
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://www.pngkey.com/png/full/408-4087034_large-format-silk-screen-printing-fashion-t-shirt.png",
+    },
+    {
+      id:2,
+      img:"https://www.pngitem.com/pimgs/m/330-3303997_shirt-png-transparent-image-folded-clothes-png-png.png",
+    },
+    {
+      id:3,
+      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+      id:4,
+      img:"https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+    },
+    {
+      id:5,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:6,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+      id:7,
+      img:"https://png.pngitem.com/pimgs/s/174-1745833_sardar-garments-fast-shipping-trendy-apparel-polo-shirt.png",
+    },
+    {
+      id:8,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+  ]
